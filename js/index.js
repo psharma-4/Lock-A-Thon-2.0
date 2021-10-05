c9438ccb4d1e68856069e85a6a6e3e434a8c0bb3
Kartavya Shankar <kartavyashankar@gmail.com>
2021-10-06 03:41:37 +0530

    Updated Cards Format

--- js/index.js
+++ js/index.js
@@ -1,22 +1,2 @@
-var cards = document.querySelectorAll(".card");
+var cards = document.querySelectorAll(".resultCard");
 
-cards[0].addEventListener("mouseover", () => {
-    cards[0].childNodes[1].classList.toggle("show");
-    cards[0].childNodes[1].classList.toggle("hide");
-    cards[0].childNodes[2].classList.toggle("hide");
-    cards[0].childNodes[2].classList.toggle("show");
-});
-
-cards[1].addEventListener("mouseover", () => {
-    cards[1].childNodes[1].classList.toggle("show");
-    cards[1].childNodes[1].classList.toggle("hide");
-    cards[1].childNodes[2].classList.toggle("hide");
-    cards[1].childNodes[2].classList.toggle("show");
-});
-
-cards[2].addEventListener("mouseover", () => {
-    cards[2].childNodes[1].classList.toggle("show");
-    cards[2].childNodes[1].classList.toggle("hide");
-    cards[2].childNodes[2].classList.toggle("hide");
-    cards[2].childNodes[2].classList.toggle("show");
-});
