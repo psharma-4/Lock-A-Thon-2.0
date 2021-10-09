1f5adbf40dc0840490c9c60607d640e487a51f16
Pranav Sharma <pranavsharma2126@gmail.com>
2021-10-09 16:48:29 +0530

    navbar added

--- js/schedule.js
+++ js/schedule.js
@@ -13,3 +13,24 @@ for( j=0;j<faqCard.length;j++){
         console.log(answer)
     })
 }
+
+
+
+const navBar=document.querySelector('.navbar')
+var navbarBrand=document.querySelector('.navbar-brand')
+window.addEventListener('scroll',()=>{
+  var scroll=window.scrollY;
+  console.log(scroll)
+  if(scroll>500){
+    navbarBrand.innerHTML='<img src="./images/robolution.jpg" alt="" style="width: 60px;" />'
+    navBar.classList.add('navbar-dark')
+    // navBar.classList.add('bg-dark')
+    navBar.classList.add('fixed-top')
+  }
+  else if(scroll<500){
+    navbarBrand.innerHTML='<img src="./images/bit.png" alt="" style="width: 60px;" />'
+    navBar.classList.remove('navbar-dark')
+    // navBar.classList.remove('bg-dark')
+    navBar.classList.remove('fixed-top')
+  }
+})
